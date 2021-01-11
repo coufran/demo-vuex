c584472c734f5f0bb3d17140fe96b204a57a4c92
coufran <coufran@qq.com>
2021-01-11 15:03:36 +0800

    state mapState 1

--- src/main.js
+++ src/main.js
@@ -1,8 +1,26 @@
 import Vue from 'vue'
+import Vuex from 'vuex'
 import App from './App.vue'
 
 Vue.config.productionTip = false
 
+Vue.use(Vuex)
+
+const store = new Vuex.Store({
+  state: {
+    count: 0
+  },
+  mutations: {
+    increment(state) {
+      state.count++;
+    }
+  }
+});
+
+store.commit("increment");
+console.log(store.state.count);
+
 new Vue({
   render: h => h(App),
+  store: store
 }).$mount('#app')
